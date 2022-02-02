7e54cd3bf1e016b422e962166d19672ca4ab67c4
Daniel Friskey <54420124+danielfriskey23@users.noreply.github.com>
2022-02-02 09:27:41 -0600

    Create post.php

---
+++ post.php
@@ -0,0 +1,15 @@
+<?php
+    ini_set('display_errors', 1);
+  	ini_set('display_startup_errors', 1);
+	  error_reporting(E_ALL);	
+    
+    date_default_timezone_set("America/Chicago");
+    
+		$THISPLAYER = $_POST['THISPLAYER'];
+		$VALUEX = $_POST['THINGX'];
+		$VALUEY = $_POST['THINGY'];
+    //DO ACTIONS WITH INFORMATION
+    
+    echo 'Success';
+    
+?>
